20589fede50f4e4a9ad4816019ad22bfc0879cb3
prsndp <prasandeepp@gmail.com>
2022-12-28 14:24:53 +0530

    set up done of project structure and nodejs server file

---
+++ server.js
@@ -0,0 +1,20 @@
+const express =  require('express');
+const socketio = require('socket.io')
+const http = require('http')
+const router = require('./router');
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket)=>{
+    console.log('We have a new connection!!!');
+    socket.on('disconnected', ()=>{
+        console.log('User had left!');
+    });
+});
+
+app.use(router);
+
+server.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './chat.css';
+
+const Chat = () => {
+  return (
+    <div className='container'>
+        <div className='chat-box'>
+            This is chat box
+            
+        </div>
+    </div>
+  )
+}
+
+export default Chat
