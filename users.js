c7b52172a5ae104b7a5b2e5a97c2f07303d78a15
prsndp <prasandeepp@gmail.com>
2022-12-28 16:10:03 +0530

    users.js file created to handling to users functionality!

---
+++ users.js
@@ -0,0 +1,28 @@
+const users = [];
+
+const addUser = ({ id, name, room }) =>{
+// jabvascript by me =  javascript by others
+name = name.trim().toLowerCase();
+room = room.trim().toLowerCase();
+
+const existngUser = users.find((user)  => user.room === room && user.name ===  name);
+    if(existngUser)
+        {
+            return { error:'Username is taken' }
+        }
+        const user = { id, name , room };
+        users.push(user);
+    return { user }
+    }
+
+const removeUser = () =>{
+    const index = users.findIndex((user) =>{
+
+    )}
+const getUser = () =>{
+
+}
+
+const getUsersRoom = () =>{
+
+}
